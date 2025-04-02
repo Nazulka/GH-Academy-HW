a056c11ddc233c8ed26b17d20d7997d089cf383b
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-02 20:15:33 +0100

    Update Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,4 @@
+
 public class Calculator {
     // Method to add two numbers
     public int add(int a, int b) {
@@ -8,4 +9,21 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
-}
+
+    public static void main(String[] args) {
+        String studentName = "Tim C";
+        int studentId = 12;
+        int studentAge= 31;
+        float studentFee = 75.56F;
+        char studentGrade = 'A';
+
+        System.out.println(studentName);
+        System.out.println(studentId);
+        System.out.println(studentAge);
+        System.out.println(studentFee);
+        System.out.println(studentGrade);
+
+
+    }
+
+    }
--- src/Loops.java
+++ src/Loops.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
 public class Loops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+            System.out.println(x);
+            System.out.println(s1);
+        }
+
+    }
 
 
 
-}
+    }
 
