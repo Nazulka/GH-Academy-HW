058fd57d5a46f51db6815bd4fe21c49a5f605eb4
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-10 21:57:58 +0100

    Add revised solution for problem 9

--- src/problems/Problem9.java
+++ src/problems/Problem9.java
@@ -6,7 +6,7 @@ public class Problem9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int result  = (a % 2 == 0) ? a + 2 : a + 1;
-           System.out.println(result);
+        int result  = (a/2+1)*2;
+        System.out.println(result);
     }
 }
