844e4cf518f8c0b1fb43041fb857f1eaddb9ea74
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-11 20:27:54 +0100

    Add solution to problem 10

---
+++ src/problems/Problem10.java
@@ -0,0 +1,14 @@
+//package problems;
+
+import java.util.Scanner;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int time = (n/60)%24;
+        int minutes = n%60;
+        System.out.println(time + " " + minutes);
+    }
+
+}
