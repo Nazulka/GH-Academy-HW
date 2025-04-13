b0a0dfc165e6631b32404796e79c7bb08e104e6c
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-13 12:17:49 +0100

    Add solution to problem 2949

---
+++ src/problems/Problem2950.java
@@ -0,0 +1,13 @@
+package problems;
+
+import java.util.Scanner;
+
+public class Problem2950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+    }
+
+}
