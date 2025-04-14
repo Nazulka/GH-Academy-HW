d70188894f745693c675ae32dcce1dc9d92cdbeb
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-14 20:10:47 +0100

    Add solution to problem 2951

--- src/problems/Problem2950.java
+++ src/problems/Problem2950.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class Problem2950 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int pounds = sc.nextInt();
+        int pence = sc.nextInt();
+        int n = sc.nextInt();
+        int total = (pounds * 100 + pence)*n;
+        int totalPounds = total / 100;
+        int totalPence = total % 100;
 
+        System.out.println(totalPounds + " " + totalPence);
     }
-
 }
