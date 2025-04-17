291cdc1e1c96311d620e31de390c44247f216247
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-17 22:09:55 +0100

    Add solution to Problem 2960

---
+++ src/problems/Problem2960.java
@@ -0,0 +1,20 @@
+package problems;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Problem2960 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if
+            (b == a){
+                System.out.println("YES");
+        }
+        System.out.println("NO");
+
+    }
+}
