abe6d65817edee9b86186a2b417f47ee6f1e58f1
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-18 19:34:18 +0100

    Add solution 293

---
+++ src/problems/Problem293.java
@@ -0,0 +1,18 @@
+//package problems;
+
+import java.util.Scanner;
+
+public class Problem293 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        if (a > b) {
+            System.out.println("1");
+        } else if (b > a) {
+            System.out.println("2");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
