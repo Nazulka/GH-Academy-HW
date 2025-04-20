0b6fdb620afa06b36e8d041e40e2716fe2799b02
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-20 14:27:59 +0100

    Add solution to Problem258

---
+++ src/problems/Problem258.java
@@ -0,0 +1,18 @@
+package problems;
+
+import java.util.Scanner;
+
+public class Problem258 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        if ((k % n == 0 && k / n <= m) || (k % m == 0 && k / m <= n)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    sc.close();
+    }
+}
