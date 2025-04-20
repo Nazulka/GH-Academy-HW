acc781b222a108acc41bff05e71028535df61e99
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-20 18:27:53 +0100

    Fix the solution

--- src/problems/Problem259.java
+++ src/problems/Problem259.java
@@ -6,7 +6,6 @@ public class Problem259 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
-        int n = (k + 4) / 4;
         if ((k + 4) % 4 == 0 && (k + 4) / 4 >= 2) {
             System.out.println("YES");
         } else {
