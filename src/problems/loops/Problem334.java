e1d42643a37de36f3b97608c467252a479b00277
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-23 08:19:50 +0100

    Add solution to problem 334

---
+++ src/problems/loops/Problem334.java
@@ -0,0 +1,18 @@
+package problems.loops;
+
+import java.util.Scanner;
+
+public class Problem334 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        for (int i = a; i <= b; i++) {
+            if (i % d == c) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
