8a267c6f8f929f422326734c363c4537edc8d6a1
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-04-28 21:23:28 +0100

    Add solution for Problem351

---
+++ src/problems/loops/Problem351.java
@@ -0,0 +1,16 @@
+package problems.loops;
+
+import java.util.Scanner;
+
+public class Problem351 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int factorial=1;
+        for (int i = 1; i <=n; i++) {
+            factorial *= i;
+
+        }
+        System.out.println(factorial);
+    }
+}
