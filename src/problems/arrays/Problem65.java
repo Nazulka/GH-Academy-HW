5fb2c987c201ddf90687e1a04404b2bc6ad73cbe
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-05-08 20:29:59 +0100

    Solution for Problem65

---
+++ src/problems/arrays/Problem65.java
@@ -0,0 +1,24 @@
+package problems.arrays;
+
+import java.util.Scanner;
+
+public class Problem65 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] > 0) {
+                sum++;
+
+            }
+        }
+        System.out.println(sum);
+    }
+}
