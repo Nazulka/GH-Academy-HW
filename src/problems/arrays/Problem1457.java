a13ce9f032c40a5da8f60cb696706d97696f5bb1
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-05-17 07:59:52 +0100

    Solve Problem 1457

---
+++ src/problems/arrays/Problem1457.java
@@ -0,0 +1,21 @@
+package problems.arrays;
+
+import java.util.Scanner;
+
+public class Problem1457 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i+1;
+        }
+
+
+    }
+}
