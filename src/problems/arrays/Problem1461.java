a1379f6b98a580e8c7704c7ea9e1d0943d74d5b0
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-03 05:44:53 +0100

    Refactor solution to 1461

--- src/problems/arrays/Problem1461.java
+++ src/problems/arrays/Problem1461.java
@@ -8,50 +8,45 @@ public class Problem1461 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();  // Number of test cases
-        for (int t = 0; t < T; t++) {
-            int n = sc.nextInt();
-            List<Integer> balls = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                balls.add(sc.nextInt());
-            }
-
-            int totalDestroyed = countDestroyedBalls(balls);
-            System.out.println(totalDestroyed);
+        int n = sc.nextInt();
+        List<Integer> balls = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            balls.add(sc.nextInt());
         }
+
+        int totalDestroyed = countDestroyedBalls(balls);
+        System.out.println(totalDestroyed);
     }
 
     private static int countDestroyedBalls(List<Integer> balls) {
         int totalDestroyed = 0;
 
-        while (true) {
-            boolean removedAny = false;
+        boolean keepChecking = true;
+        while (keepChecking) {
+            keepChecking = false;
             int i = 0;
 
             while (i < balls.size()) {
                 int j = i + 1;
 
+                // Expand j to find the end of the current group
                 while (j < balls.size() && balls.get(j).equals(balls.get(i))) {
                     j++;
                 }
 
-                int chainLength = j - i;
+                int groupSize = j - i;
 
-                if (chainLength >= 3) {
+                if (groupSize >= 3) {
+                    // Remove the group from right to left
                     for (int k = j - 1; k >= i; k--) {
                         balls.remove(k);
                     }
-                    totalDestroyed += chainLength;
-                    removedAny = true;
-                    break;  // restart after modification
+                    totalDestroyed += groupSize;
+                    keepChecking = true;
+                    break; // Restart entire process from beginning
                 } else {
                     i = j;
                 }
-            }
-
-            if (!removedAny) {
-                break;
             }
         }
 
